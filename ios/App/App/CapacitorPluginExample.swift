3510c9af9c0395ea1bfa33f184bb77336a6506eb
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-08-04 13:03:17 -0400

    update obj

--- ios/App/App/CapacitorPluginExample.swift
+++ ios/App/App/CapacitorPluginExample.swift
@@ -1,6 +1,6 @@
 import Capacitor
 
-@objc(EchoPlugin)
+@objc(CapacitorPluginExample)
 public class CapacitorPluginExample: CAPPlugin {
     @objc func echo(_ call: CAPPluginCall) {
         let value = call.getString("value") ?? ""
