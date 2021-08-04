41145f7695a08bfd1051a84687444d34c7f59f0f
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-08-04 10:22:06 -0400

    plugin exmaple

---
+++ android/app/src/main/java/app/footycity/app/plugins/CapacitorPluginExample.kt
@@ -0,0 +1,21 @@
+
+package app.footycity.app.plugins
+
+import com.getcapacitor.JSObject
+import com.getcapacitor.Plugin
+import com.getcapacitor.PluginCall
+import com.getcapacitor.PluginMethod
+import com.getcapacitor.annotation.CapacitorPlugin
+
+@CapacitorPlugin(name = "CapacitorPluginExample")
+class CapacitorPluginExample : Plugin() {
+    @PluginMethod
+    fun echo(call: PluginCall) {
+        val value = call.getString("value")
+        val ret = JSObject()
+        ret.put("value", value)
+        ret.put("source", "hello android")
+        println("hello android")
+        call.resolve(ret)
+    }
+}
