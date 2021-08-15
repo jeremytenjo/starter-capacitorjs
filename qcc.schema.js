6780996eab89006612d8f233654ccc1fff77a7f5
Jeremy Tenjo <tenjojeremy@gmail.com>
2021-08-15 19:02:39 -0400

    qcc.schema.js

--- qcc.schema.js
+++ qcc.schema.js
@@ -1,1 +1,115 @@
-module.exports = require('../../qcc.schema')
+const component = {
+  type: 'Simple Component',
+  files: [
+    {
+      path: () => `index.js`,
+      template: ({ name, helpers }) => `        
+      import * as S from './styles'
+
+      export default function ${helpers.changeCase.pascalCase(name)}() {
+        return (
+          <S.Wrapper>
+           ${name}
+          </S.Wrapper> 
+        );
+      }
+        `,
+    },
+    {
+      path: () => `styles.js`,
+      template: ({ helpers }) => `        
+      import styled from 'styled-components'
+
+      export const Wrapper = styled.div${helpers.addEmptyTemplateLiteral()}
+        `,
+    },
+  ],
+}
+
+const componentWithStory = {
+  type: 'Complete Component',
+  files: [
+    {
+      path: () => `index.js`,
+      template: ({ name }) => `        
+      import use${name}Data from './use${name}Data'
+      import Ui from './ui/ui'
+      
+      export default function ${name}(props) {
+        const data = use${name}Data({ props })
+        return <Ui {...props} {...data} />
+      }
+        `,
+    },
+    {
+      path: () => `ui/ui.js`,
+      template: ({ name, helpers }) => `        
+      import * as S from './styles'
+
+      export default function ${helpers.changeCase.pascalCase(name)}() {
+        return (
+          <S.Wrapper>
+           ${name}
+          </S.Wrapper> 
+        );
+      }
+        `,
+    },
+    {
+      path: () => `ui/styles.js`,
+      template: ({ helpers }) => `        
+      import styled from 'styled-components'
+
+      export const Wrapper = styled.div${helpers.addEmptyTemplateLiteral()}
+        `,
+    },
+    {
+      path: ({ name }) => `use${name}Data/index.js`,
+      template: ({ name }) => `        
+      export default function use${name}Data({ props }) {
+        console.log(props)
+        return {}
+      }
+        `,
+    },
+    {
+      path: () => `stories/sb.stories.mdx`,
+      template: ({ name }) => `import { Meta } from '@storybook/addon-docs/blocks'
+import ${name} from '../'
+      
+<Meta 
+  title='Lib/${name}' 
+  component={${name}} 
+  argTypes={{}} 
+  args={{}} 
+/>
+
+# ${name}
+
+export const Template = (args) => (
+  <${name} {...args}>
+    ${name}
+  </${name}>
+)
+
+<Story name='default' argTypes={{}} args={{}}>
+  {Template.bind({})}
+</Story>
+`,
+    },
+  ],
+}
+
+module.exports = [
+  component,
+  componentWithStory,
+  {
+    type: 'Function',
+    files: [
+      {
+        path: () => 'index.js',
+        template: ({ name }) => `export default function ${name}() {}`,
+      },
+    ],
+  },
+]
